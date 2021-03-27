656f310cd5f5ca3063ff79619916fae0b48c68b6
zh-lx <18366276315@163.com>
2021-03-27 20:35:45 +0800

    feat: 删除node目录

--- node/cli/packages/cli-template/postcss.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  plugins: {
-    'postcss-import': {},
-    'postcss-url': {},
-    // 一般package.json中会有browserslist的配置,这里直接写出来就好
-    autoprefixer: {},
-  },
-};
--- node/cli/packages/cli-template/src/index.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue';
-/* 主程序 */
-import App from './App.vue';
-
-const app = createApp(App);
-app.mount('#app');
